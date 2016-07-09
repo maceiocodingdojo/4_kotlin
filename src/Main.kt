0f560faab6b0148455ca17ac6b0e26aec07894f1
Bruno de Lima <dlimaun@gmail.com>
2016-07-09 11:24:04 -0300

    Lucas Pugliese

--- src/Main.kt
+++ src/Main.kt
@@ -3,9 +3,15 @@ import java.util.*
 
 class PassaTroco{
     fun calculaTroco(valorCompra: BigDecimal, valorRecebido: BigDecimal): List<BigDecimal> {
+        var diferenca : BigDecimal = valorRecebido - valorCompra;
+        var listDeTroco : List<BigDecimal>? = null;
+
         if(valorCompra.compareTo(valorRecebido) != 0){
             return listOf(BigDecimal.ONE)
         }
+
+
+
         return listOf<BigDecimal>()
     }
 
