9dbd636a8eb758a2c839d911edae42d884c3bc55
Bruno de Lima <dlimaun@gmail.com>
2016-07-09 12:50:38 -0300

    Jose Vieira

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,9 @@ class PassaTroco{
     companion object{
         val UM_REAL = 1
         val CINCO_REAIS = 5
+        val DEZ_REAIS = 10
+        val CINQUENTA_REAIS = 50
+        val CEM_REAIS = 100
     }
 
     fun calculaTroco(valorCompra: BigDecimal, valorRecebido: BigDecimal): Map<Int, Int> {
@@ -13,17 +16,20 @@ class PassaTroco{
 
         if(valorRecebido > valorCompra){
 
-            return mapOf(getMaiorCedula(diferenca) to 1)
+            //return mapOf(getMaiorCedula(diferenca) to 1)
         }
 
-        getMaiorCedula(diferenca)
+       // getMaiorCedula(diferenca)
 
         return listDeTroco
     }
 
-    private fun getMaiorCedula(diferenca: BigDecimal) : Int{
+    private fun calcularMenorQua(diferenca: BigDecimal) : Int{
+
+
 
         return when(diferenca){
+
             BigDecimal("1") -> UM_REAL
             BigDecimal("5") -> CINCO_REAIS
             else -> 0
