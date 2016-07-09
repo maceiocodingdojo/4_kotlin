5e1755da5394effc98fad3c5d69ae031eca2f727
Bruno de Lima <dlimaun@gmail.com>
2016-07-09 10:26:51 -0300

    Primeiro teste Romane

---
+++ src/Test.kt
@@ -0,0 +1,33 @@
+import java.math.BigDecimal
+
+//class SimpleTest : Spek({
+//    describe("a calculator") {
+//        val calculator = SampleCalculator()
+//
+//        it("should return the result of adding the first number to the second number") {
+//            val sum = calculator.sum(2, 4)
+//            assertEquals(6, sum)
+//        }
+//
+//        it("should return the result of subtracting the second number from the first number") {
+//            val subtract = calculator.subtract(4, 2)
+//            assertEquals(2, subtract)
+//        }
+//    }
+//})
+
+class TrocoTest : Spek({
+    describe("um passador de troco") {
+        val passaTroco = PassaTroco()
+
+        it("sem troco, deve retornar uma list vazia") {
+            val troco= passaTroco.calculaTroco(BigDecimal("10"), BigDecimal("10"))
+            assertEquals(troco,listOf<BigDecimal>() )
+        }
+
+        it("should return the result of subtracting the second number from the first number") {
+            val subtract = calculator.subtract(4, 2)
+            assertEquals(2, subtract)
+        }
+    }
+})
