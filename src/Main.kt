0c7523926db33c7de77cf919263c2d5fa857d307
Bruno de Lima <dlimaun@gmail.com>
2016-07-09 11:15:26 -0300

    Ivan Marreta

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,11 @@
 import java.math.BigDecimal
+import java.util.*
 
 class PassaTroco{
-    fun calculaTroco(valorCompra: BigDecimal, valorRecebido: BigDecimal): Any? {
-
+    fun calculaTroco(valorCompra: BigDecimal, valorRecebido: BigDecimal): List<BigDecimal> {
+        if(valorCompra.compareTo(valorRecebido) != 0){
+            return listOf(BigDecimal.ONE)
+        }
         return listOf<BigDecimal>()
     }
 
--- src/Test.kt
+++ src/Test.kt
@@ -2,32 +2,18 @@ import junit.framework.Assert.*
 import org.jetbrains.spek.api.Spek
 import java.math.BigDecimal
 
-//class SimpleTest : Spek({
-//    describe("a calculator") {
-//        val calculator = SampleCalculator()
-//
-//        it("should return the result of adding the first number to the second number") {
-//            val sum = calculator.sum(2, 4)
-//            assertEquals(6, sum)
-//        }
-//
-//        it("should return the result of subtracting the second number from the first number") {
-//            val subtract = calculator.subtract(4, 2)
-//            assertEquals(2, subtract)
-//        }
-//    }
-//})
-
 class TrocoTest : Spek({
     describe("um passador de troco") {
         val passaTroco = PassaTroco()
 
         it("sem troco, deve retornar uma list vazia") {
-            val troco= passaTroco.calculaTroco(BigDecimal("10"), BigDecimal("10"))
-            assertEquals(troco,listOf<BigDecimal>())
+            val troco = passaTroco.calculaTroco(BigDecimal("10"), BigDecimal("10"))
+            assertEquals(troco, listOf<BigDecimal>())
         }
-        it("Agora temos troco"){
-            val
+
+        it("com troco devemos retorna uma lista com pelo menos um valor"){
+            val troco = passaTroco.calculaTroco(BigDecimal("10"), BigDecimal("15"))
+            assertEquals(troco.isNotEmpty(), true)
         }
     }
 })
